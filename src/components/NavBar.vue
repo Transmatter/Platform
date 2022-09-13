724a36779cca823718ca2728c378cf3646d2834d
oat431 <oat431@gmail.com>
2022-09-13 18:58:36 +0700

    [🚚 refactor] : from option api to composition api

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,69 +1,60 @@
-<script>
+<script setup>
 import LinkButton from '@/widget/LinkButton.vue';
 import ProfileIcon from '@/widget/ProfileIcon.vue';
 import Buttom from '@/widget/Buttom.vue';
-import KeyBoardEvent from './KeyBoardEvent.vue'
-export default {
-    
-    components: {
-    LinkButton,
-    ProfileIcon,
-    Buttom,
-    KeyBoardEvent,
-},
-    methods:{
-        mockLogin(){
-            this.$router.push('/login');
-        },
-        mockRegister(){
-            this.$router.push('/register');
-        },
-        handleKeyPress: function (e) {
-        const keyCode = String(e.keyCode || e.code || e.keyIdentifier);
-        if(keyCode == '37'){
-            window.history.back()
-        }else if(keyCode == '39'){
-            window.history.forward()
-        }else if(keyCode=='32'){
-            document.getElementById("searchBox").focus();
-        }else if(keyCode=='13'){
-            document.getElementById("searchButt").click();
-        }else if(keyCode== '90'){
-            if(document.getElementById("sugg1")){
-                document.getElementById("sugg1").click();
-            }
-            
-        }else if(keyCode=='88'){
-            if(document.getElementById("sugg2")){
-                document.getElementById("sugg2").click();
-            }
-        }else if(keyCode=='67'){
-             if(document.getElementById("sugg3")){
-                document.getElementById("sugg3").click();
-            }
-        }else if(keyCode == '38'){
-            this.$router.push({ name: "Home" })
-        }
+import { onKeyStroke } from '@vueuse/core';
+import { useRouter } from 'vue-router';
 
-    }
-    } 
+const router = useRouter();
+const mockLogin = () => {
+    this.$router.push('/login');
 };
+const mockRegister = () => {
+    this.$router.push('/register');
+};
+
+onKeyStroke('ArrowUp', (e) => {
+    router.push({ name: 'Home' });
+});
+
+onKeyStroke('ArrowLeft', (e) => {
+    window.history.back();
+});
+
+onKeyStroke('ArrowRight', (e) => {
+    window.history.forward();
+});
+
+onKeyStroke('Control', (e) => {
+    document.getElementById('searchBox').focus();
+});
+
+onKeyStroke('Enter', (e) => {
+    document.getElementById('searchButt').click();
+});
+
+onKeyStroke(['z', 'Z'], (e) => {
+    document.getElementById('sugg1').click();
+});
+
+onKeyStroke(['x', 'X'], (e) => {
+    document.getElementById('sugg2').click();
+});
+
+onKeyStroke(['c', 'C'], (e) => {
+    document.getElementById('sugg3').click();
+});
 </script>
 <template>
-<KeyBoardEvent v-on:keyup="handleKeyPress"></KeyBoardEvent>
     <div>
         <nav class="bg-white shadow">
             <div class="max-w-9xl mx-auto px-8">
                 <div class="flex items-center justify-between h-16">
                     <div class="flex items-center">
-
-                        <a class="flex-shrink-0" href="/" @click.left="handleKeyPress">
-                             <h1 class="text-primary text-xl font-bold normal-case">
-
-                                Transmatter Platform 
-                                <sup class="text-xs" v-if="this.$store.getters.getRole == 'ROLE_ADMIN'">
-                                    Admin
-                                </sup>
+                        <a class="flex-shrink-0" href="/">
+                            <h1 class="text-primary text-xl font-bold normal-case">
+                                Transmatter Platform
+                                <sup class="text-xs" v-if="this.$store.getters.getRole == 'ROLE_ADMIN'"> Admin </sup>
                                 <sup class="text-xs" v-if="this.$store.getters.getRole == 'ROLE_SUPER_ADMIN'">
                                     SAdmin
                                 </sup>
@@ -72,23 +63,23 @@ export default {
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
                                 <span>
-                                    <LinkButton path="Home"/>
-                                    <LinkButton path="About"/>
+                                    <LinkButton path="Home" />
+                                    <LinkButton path="About" />
                                 </span>
                             </div>
                         </div>
                     </div>
-                    <ProfileIcon v-if="this.$store.getters.getCurrentUser != null"/>
+                    <ProfileIcon v-if="this.$store.getters.getCurrentUser != null" />
                     <span v-else>
-                        <Buttom class="mx-2" @click="mockRegister" buttonName="Sign up"/>
-                        <Buttom class="mx-2" @click="mockLogin" buttonName="Sign in"/>
+                        <Buttom class="mx-2" @click="mockRegister" buttonName="Sign up" />
+                        <Buttom class="mx-2" @click="mockLogin" buttonName="Sign in" />
                     </span>
                 </div>
             </div>
             <div class="md:hidden">
                 <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                    <LinkButton path="Home"/>
-                    <LinkButton path="About"/>
+                    <LinkButton path="Home" />
+                    <LinkButton path="About" />
                 </div>
             </div>
         </nav>
